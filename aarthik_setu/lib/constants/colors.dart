df2e463a3f9978dce788a4f6ac10eb9b2779f5dd
nailsonseat <aadarshv3@gmail.com>
2024-08-29 02:20:17 +0530

    Added constants

---
+++ aarthik_setu/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static Color primaryColorOne = HexColor('F36B26');
+  static Color primaryColorTwo = HexColor('B8CB1B');
+}
