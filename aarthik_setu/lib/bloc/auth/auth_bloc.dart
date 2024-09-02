7c3c422d8b9c1ac4c564e19c9b17c7d18070a765
nailsonseat <aadarshv3@gmail.com>
2024-09-03 01:47:42 +0530

    Auth bloc with google sign in events

--- aarthik_setu/lib/bloc/auth/auth_bloc.dart
+++ aarthik_setu/lib/bloc/auth/auth_bloc.dart
@@ -1,13 +1,26 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:aarthik_setu/services/auth/google.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/logger.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final GoogleAuth googleAuthServices;
+  final _logger = Logger();
+
+  AuthBloc({required this.googleAuthServices}) : super(AuthInitial()) {
+    on<GoogleSignIn>((event, emit) {
+      emit(AuthLoading());
+      try {
+        final response = googleAuthServices.signInGoogle();
+        _logger.i(response);
+        emit(AuthSuccess());
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
     });
   }
 }
--- aarthik_setu/lib/bloc/auth/auth_event.dart
+++ aarthik_setu/lib/bloc/auth/auth_event.dart
@@ -2,3 +2,7 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+class GoogleSignIn extends AuthEvent {}
+
+class OTPSignIn extends AuthEvent {}
