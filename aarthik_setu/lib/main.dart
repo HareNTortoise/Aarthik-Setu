e4febfc91388b000813c3c1e4a5e8d073a585ec1
nailsonseat <aadarshv3@gmail.com>
2024-09-03 01:47:58 +0530

    Update main

--- aarthik_setu/lib/main.dart
+++ aarthik_setu/lib/main.dart
@@ -10,14 +10,15 @@ import 'constants/app_constants.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const AarthikSetu());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AarthikSetu extends StatelessWidget {
+  const AarthikSetu({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +28,8 @@ class MyApp extends StatelessWidget {
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(create: (context) => AuthBloc()),
+          BlocProvider(create: (context) => AuthBloc(googleAuthServices: RepositoryProvider.of<GoogleAuth>(context))),
+
         ],
         child: MaterialApp.router(
           title: AppConstants.appName,
@@ -45,54 +47,6 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
