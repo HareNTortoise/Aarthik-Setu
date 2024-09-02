bcfaf1356d490f6fa1d58d888daf661522d12686
nailsonseat <aadarshv3@gmail.com>
2024-09-03 01:29:55 +0530

    Update app constants

--- aarthik_setu/lib/constants/app_constants.dart
+++ aarthik_setu/lib/constants/app_constants.dart
@@ -1,3 +1,7 @@
 class AppConstants {
   static const String appName = 'Aarthik Setu';
+  static const double desktopScaleWidth = 1980;
+  static const double mobileScaleWidth = 450;
+
+  static Duration otpTimeOutDuration = const Duration(minutes: 2);
 }
