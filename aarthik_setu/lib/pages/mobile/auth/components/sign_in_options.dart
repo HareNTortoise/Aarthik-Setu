128992e0c0698476165d7feccb5cbd4d98d71c0c
nailsonseat <aadarshv3@gmail.com>
2024-09-08 02:19:51 +0530

    Sign in options for mobile, with cubit

---
+++ aarthik_setu/lib/pages/mobile/auth/components/sign_in_options.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../bloc/auth/auth_bloc.dart';
+
+class SignInOptionsMobile extends StatelessWidget {
+  const SignInOptionsMobile({super.key, required this.onPhoneSignIn});
+
+  final VoidCallback? onPhoneSignIn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Text('Sign In', style: GoogleFonts.poppins(fontSize: 40, fontWeight: FontWeight.w400)),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {
+              context.read<AuthBloc>().add(GoogleSignIn());
+            },
+            style: OutlinedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              minimumSize: const Size(double.infinity, 40),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "lib/assets/google.svg",
+                  width: 30,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  "Google",
+                  style: GoogleFonts.jost(fontSize: 26, color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: onPhoneSignIn,
+            style: OutlinedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              minimumSize: const Size(double.infinity, 40),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "lib/assets/phone_sms.svg",
+                  width: 30,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  "Phone",
+                  style: GoogleFonts.jost(fontSize: 26, color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
