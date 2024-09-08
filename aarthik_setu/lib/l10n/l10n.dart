e07e6b8cd4edf50bda0f5b6fc91be7d874740371
nailsonseat <aadarshv3@gmail.com>
2024-09-09 02:26:33 +0530

    L10n config

---
+++ aarthik_setu/lib/l10n/l10n.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class L10nAll{
+  static const all =[
+    Locale('en'),
+    Locale('hi'),
+    Locale('bn'),
+    Locale('kn'),
+    Locale('ta'),
+    Locale('te'),
+  ];
+  static const languageNames = {
+    'en': 'English',
+    'hi': 'हिंदी',
+    'bn': 'বাংলা',
+    'kn': 'ಕನ್ನಡ',
+    'ta': 'தமிழ்',
+    'te': 'తెలుగు',
+  };
+}
