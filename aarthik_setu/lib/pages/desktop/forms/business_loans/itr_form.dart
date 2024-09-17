bdab35378c748a7685e06c7ddc90f860d72cd984
nailsonseat <aadarshv3@gmail.com>
2024-09-18 01:15:11 +0530

    ITR form for business loans

---
+++ aarthik_setu/lib/pages/desktop/forms/business_loans/itr_form.dart
@@ -0,0 +1,50 @@
+import 'package:aarthik_setu/constants/app_constants.dart';
+import 'package:aarthik_setu/pages/desktop/forms/personal_loans/components/itr_file.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class ITRFormBusiness extends StatefulWidget {
+  const ITRFormBusiness({super.key});
+
+  @override
+  State<ITRFormBusiness> createState() => _ITRFormBusinessState();
+}
+
+class _ITRFormBusinessState extends State<ITRFormBusiness> {
+  bool _isManual = true;
+  int currentYear = DateTime.now().year;
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveScaledBox(
+      width: AppConstants.desktopScaleWidth,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 100.0),
+          child: SizedBox.expand(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 60),
+                  Text(
+                    "ITR Details",
+                    style: GoogleFonts.poppins(fontSize: 80),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Upload ITR upto 3 years",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 60),
+                  const ItrFilePersonal() ,
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
