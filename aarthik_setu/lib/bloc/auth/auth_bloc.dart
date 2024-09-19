4a3529abca87d3207cbf53f383bea37f9f0351d6
nailsonseat <aadarshv3@gmail.com>
2024-09-20 01:00:19 +0530

    Setup bloc to handle google login properly

--- aarthik_setu/lib/bloc/auth/auth_bloc.dart
+++ aarthik_setu/lib/bloc/auth/auth_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:aarthik_setu/services/auth/google.dart';
 import 'package:aarthik_setu/services/auth/phone.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
@@ -14,37 +17,69 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final _logger = Logger();
 
   AuthBloc({required this.googleAuthServices, required this.phoneAuthServices}) : super(AuthInitial()) {
-    on<GoogleSignIn>((event, emit) {
-      emit(AuthLoading());
+    on<GoogleSignIn>((event, emit) async {
+      emit(Loading());
       try {
-        final response = googleAuthServices.signInGoogle();
-        _logger.i(response);
-        emit(AuthSuccess());
+        Map<String, dynamic>? response  = await googleAuthServices.signInGoogle();
+        if (response == null) {
+          emit(AuthPending());
+          return;
+        }
+        emit(AuthSuccess('User is logged in. ${response['name']}'));
       } catch (e) {
-        emit(AuthFailure(e.toString()));
+        Logger().e(e);
+        emit(AuthPending());
+        return;
       }
     });
 
+    on<AuthCheck>(
+      (event, emit) async {
+        final userCompleter = Completer<User?>();
+
+        googleAuthServices.getCurrentUser().listen((user) {
+          if (user != null) {
+            userCompleter.complete(user);
+          }
+        });
+
+        final user = await userCompleter.future.timeout(const Duration(seconds: 2), onTimeout: () => null);
+
+
+        if (user != null) {
+          emit(AuthSuccess('User is logged in. ${user.displayName}'));
+        } else {
+          emit(AuthPending());
+        }
+      },
+    );
+
     on<OTPSignIn>((event, emit) {
-      emit(AuthLoading());
+      emit(Loading());
       try {
         final response = phoneAuthServices.signInOTP(event.countryCode, event.phoneNumber);
         _logger.i(response);
-        emit(AuthSuccess());
+        emit(AuthSuccess(
+            'OTP sent to ${event.countryCode} ${event.phoneNumber}. ${FirebaseAuth.instance.currentUser!.phoneNumber}'));
       } catch (e) {
         emit(AuthFailure(e.toString()));
       }
     });
 
     on<OTPVerify>((event, emit) {
-      emit(AuthLoading());
+      emit(Loading());
       try {
         final response = phoneAuthServices.verifyOTP(event.otp);
         _logger.i(response);
-        emit(AuthSuccess());
+        emit(AuthSuccess('User is logged in. ${FirebaseAuth.instance.currentUser!.phoneNumber}'));
       } catch (e) {
         emit(AuthFailure(e.toString()));
       }
+    });
+
+    on<SignOut>((event, emit) async {
+      await googleAuthServices.signOut();
+      emit(AuthPending());
     });
   }
 }
--- aarthik_setu/lib/bloc/auth/auth_state.dart
+++ aarthik_setu/lib/bloc/auth/auth_state.dart
@@ -5,9 +5,15 @@ sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
 
-class AuthLoading extends AuthState {}
+class Loading extends AuthState {}
 
-class AuthSuccess extends AuthState {}
+class AuthPending extends AuthState {}
+
+class AuthSuccess extends AuthState {
+  final String message;
+
+  AuthSuccess(this.message);
+}
 
 class AuthFailure extends AuthState {
   final String message;
