eabb26191a698a4397d358b3ad56b0c40ee3cc67
nailsonseat <aadarshv3@gmail.com>
2024-09-22 15:42:43 +0530

    Account for all loan types in the template

--- aarthik_setu/lib/pages/desktop/forms/template/loan_form_journey_template.dart
+++ aarthik_setu/lib/pages/desktop/forms/template/loan_form_journey_template.dart
@@ -1,17 +1,21 @@
+import 'package:aarthik_setu/pages/desktop/forms/template/business_loan_journey.dart';
+import 'package:aarthik_setu/pages/desktop/forms/template/personal_loan_journey.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-import '../../../constants/app_constants.dart';
+import '../../../../constants/app_constants.dart';
 
 class LoanFormJourneyTemplate extends StatelessWidget {
-  const LoanFormJourneyTemplate({super.key, required this.formTitle, this.formDescription, required this.formLinks});
+  const LoanFormJourneyTemplate({super.key, required this.formTitle, this.formDescription, required this.loanType});
 
   final String formTitle;
   final String? formDescription;
-  final List<Widget> formLinks;
+  final String? loanType;
 
   @override
   Widget build(BuildContext context) {
+    List<String> personalLoanTypes = ['personal', 'home', 'auto'];
+    List<String> businessLoanTypes = ['msme', 'term', 'mudra'];
     return ResponsiveScaledBox(
       width: AppConstants.desktopScaleWidth,
       child: Scaffold(
@@ -31,14 +35,10 @@ class LoanFormJourneyTemplate extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 40),
-                  SizedBox(
-                    width: double.infinity,
-                    child: Wrap(
-                      runSpacing: 100,
-                      alignment: WrapAlignment.spaceAround,
-                      children: formLinks
-                    ),
-                  ),
+                  if (personalLoanTypes.contains(loanType))
+                    const SizedBox(child: PersonalLoanJourney())
+                  else if (businessLoanTypes.contains(loanType))
+                    const SizedBox(child: BusinessLoanJourney()),
                   const SizedBox(height: 100),
                 ],
               ),
