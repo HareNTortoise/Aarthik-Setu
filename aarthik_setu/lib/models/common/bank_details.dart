b151c116d2fb1ef59d6fa2d859a1c932c15b5370
nailsonseat <aadarshv3@gmail.com>
2024-09-26 15:02:00 +0530

    Refactor personal forms

--- aarthik_setu/lib/models/common/bank_details.dart
+++ aarthik_setu/lib/models/common/bank_details.dart
@@ -1,32 +1,47 @@
-
-class BankDetails{
+class BankDetails {
+  String? id;
+  String profileId;
+  DateTime timestamp;
+  String applicationId;
   String bankName;
   bool salaryAccount;
   DateTime accountSince;
   List<String> bankAccountStatements;
 
   BankDetails({
+    required this.profileId,
     required this.bankName,
     required this.salaryAccount,
     required this.accountSince,
     required this.bankAccountStatements,
+    required this.applicationId,
+    required this.timestamp,
+    this.id,
   });
 
   Map<String, dynamic> toJson() {
     return {
+      'profileId': profileId,
       'bankName': bankName,
       'salaryAccount': salaryAccount,
       'accountSince': accountSince,
-      'bankAccountStatement': bankAccountStatements,
+      'bankAccountStatements': bankAccountStatements,
+      'applicationId': applicationId,
+      'timestamp': timestamp,
+      'id': id,
     };
   }
 
   factory BankDetails.fromJson(Map<String, dynamic> json) {
     return BankDetails(
+      profileId: json['profileId'],
       bankName: json['bankName'],
       salaryAccount: json['salaryAccount'],
       accountSince: json['accountSince'],
       bankAccountStatements: json['bankAccountStatements'],
+      applicationId: json['applicationId'],
+      timestamp: json['timestamp'],
+      id: json['id'],
     );
   }
 }
