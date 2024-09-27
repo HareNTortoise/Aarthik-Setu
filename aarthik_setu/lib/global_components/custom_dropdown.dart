ad07b2c05d4561c286eb33b04d97e2aca00f33e3
nailsonseat <aadarshv3@gmail.com>
2024-09-28 00:18:45 +0530

    Added search in custom drop down

--- aarthik_setu/lib/global_components/custom_dropdown.dart
+++ aarthik_setu/lib/global_components/custom_dropdown.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomDropdown extends StatelessWidget {
-  const CustomDropdown({
+  CustomDropdown({
     super.key,
     required this.label,
-    required this.labelFontSize,
+    this.labelFontSize = 18,
     required this.buttonLabel,
+    this.searchHintText = 'Search for an item...',
     this.current,
     this.width = 300,
     this.height = 65,
@@ -22,7 +23,9 @@ class CustomDropdown extends StatelessWidget {
   final double labelFontSize;
   final String? current;
   final Function(String?)? onChanged;
-  final List<DropdownMenuItem<String>> items;
+  final List<String> items;
+  final String searchHintText;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -45,16 +48,20 @@ class CustomDropdown extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
+                      borderRadius: BorderRadius.circular(15),
                       border: Border.all(color: Colors.grey, width: 1.5),
                     ),
                     child: Row(
                       mainAxisSize: MainAxisSize.min,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          current ?? buttonLabel,
-                          style: const TextStyle(color: Colors.black, fontSize: 18),
+                        SizedBox(
+                          width: width - 100,
+                          child: Text(
+                            current ?? buttonLabel,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.poppins(color: Colors.black, fontSize: 18),
+                          ),
                         ),
                         const SizedBox(width: 10),
                         const Icon(Icons.arrow_drop_down),
@@ -63,14 +70,21 @@ class CustomDropdown extends StatelessWidget {
                   ),
                 ),
               ),
-              items: items,
+              items: items
+                  .map(
+                    (item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(item, style: GoogleFonts.poppins(color: Colors.black, fontSize: 18)),
+                    ),
+                  )
+                  .toList(),
               onChanged: onChanged,
               buttonStyleData: ButtonStyleData(
                 height: 60,
                 width: 800,
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
+                  borderRadius: BorderRadius.circular(15),
                 ),
               ),
               style: const TextStyle(color: Colors.black, fontSize: 18),
@@ -79,7 +93,7 @@ class CustomDropdown extends StatelessWidget {
                 maxHeight: 500,
                 width: 400,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
+                  borderRadius: BorderRadius.circular(15),
                 ),
                 offset: const Offset(-20, 0),
                 scrollbarTheme: ScrollbarThemeData(
@@ -87,6 +101,34 @@ class CustomDropdown extends StatelessWidget {
                   thickness: WidgetStateProperty.all(6),
                   thumbVisibility: WidgetStateProperty.all(true),
                 ),
+              ),
+              dropdownSearchData: DropdownSearchData(
+                searchController: _searchController,
+                searchInnerWidgetHeight: 50,
+                searchInnerWidget: Container(
+                  height: 90,
+                  padding: const EdgeInsets.all(15),
+                  child: TextFormField(
+                    expands: true,
+                    maxLines: null,
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 8,
+                      ),
+                      hintText: searchHintText,
+                      hintStyle: const TextStyle(fontSize: 18),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+                searchMatchFn: (item, searchValue) {
+                  return item.value.toString().toLowerCase().contains(searchValue.toLowerCase());
+                },
               ),
             ),
           ),
