e62df24710607bb50c192dd0bc31a3226b87e386
nailsonseat <aadarshv3@gmail.com>
2024-09-27 12:11:22 +0530

    Business profile

---
+++ aarthik_setu/lib/models/loan_applications/business/business_profile.dart
@@ -0,0 +1,31 @@
+class BusinessProfile {
+  final String? id;
+  final String? userId;
+  final String name;
+  final String pan;
+
+  BusinessProfile({
+    this.id,
+    this.userId,
+    required this.name,
+    required this.pan,
+  });
+
+  factory BusinessProfile.fromJson(Map<String, dynamic> json) {
+    return BusinessProfile(
+      id: json['_id'],
+      userId: json['userId'],
+      name: json['name'],
+      pan: json['pan'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'name': name,
+      'pan': pan,
+    };
+  }
+}
