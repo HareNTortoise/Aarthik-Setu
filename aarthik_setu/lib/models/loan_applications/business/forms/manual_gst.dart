32aec353c8bee2b264367ac2b5da1e2aecde9dc6
nailsonseat <aadarshv3@gmail.com>
2024-09-29 01:01:53 +0530

    GST form update and models

--- aarthik_setu/lib/models/loan_applications/business/forms/manual_gst.dart
+++ aarthik_setu/lib/models/loan_applications/business/forms/manual_gst.dart
@@ -1,4 +1,4 @@
-class ManualGST{
+class ManualGST {
   String entityName;
   DateTime dateOfIncorporation;
   String constitution;
@@ -6,8 +6,18 @@ class ManualGST{
   String city;
   String pan;
   int noOfCustomers;
-  double highestSaleToOneCustomer;
-  Map<(int,int), double> salesInLast12Months;
+  double? highestSaleToCustomerJanuary;
+  double? highestSaleToCustomerFebruary;
+  double? highestSaleToCustomerMarch;
+  double? highestSaleToCustomerApril;
+  double? highestSaleToCustomerMay;
+  double? highestSaleToCustomerJune;
+  double? highestSaleToCustomerJuly;
+  double? highestSaleToCustomerAugust;
+  double? highestSaleToCustomerSeptember;
+  double? highestSaleToCustomerOctober;
+  double? highestSaleToCustomerNovember;
+  double? highestSaleToCustomerDecember;
 
   ManualGST({
     required this.entityName,
@@ -17,34 +27,41 @@ class ManualGST{
     required this.city,
     required this.pan,
     required this.noOfCustomers,
-    required this.highestSaleToOneCustomer,
-    required this.salesInLast12Months,
+    this.highestSaleToCustomerJanuary,
+    this.highestSaleToCustomerFebruary,
+    this.highestSaleToCustomerMarch,
+    this.highestSaleToCustomerApril,
+    this.highestSaleToCustomerMay,
+    this.highestSaleToCustomerJune,
+    this.highestSaleToCustomerJuly,
+    this.highestSaleToCustomerAugust,
+    this.highestSaleToCustomerSeptember,
+    this.highestSaleToCustomerOctober,
+    this.highestSaleToCustomerNovember,
+    this.highestSaleToCustomerDecember,
   });
 
   factory ManualGST.fromJson(Map<String, dynamic> json) {
     return ManualGST(
-        entityName: json['entityName'],
-        dateOfIncorporation: DateTime.parse(json['dateOfIncorporation']),
-        constitution: json['constitution'],
-        state: json['state'],
-        city: json['city'],
-        pan: json['pan'],
-        noOfCustomers: json['noOfCustomers'],
-        highestSaleToOneCustomer: json['highestSaleToOneCustomer'],
-        salesInLast12Months: json['salesInLast12Months']);
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'entityName': entityName,
-      'dateOfIncorporation': dateOfIncorporation.toIso8601String(),
-      'constitution': constitution,
-      'state': state,
-      'city': city,
-      'pan': pan,
-      'noOfCustomers': noOfCustomers,
-      'highestSaleToOneCustomer': highestSaleToOneCustomer,
-      'salesInLast12Months': salesInLast12Months
-    };
+      entityName: json['entityName'],
+      dateOfIncorporation: DateTime.parse(json['dateOfIncorporation']),
+      constitution: json['constitution'],
+      state: json['state'],
+      city: json['city'],
+      pan: json['pan'],
+      noOfCustomers: json['noOfCustomers'],
+      highestSaleToCustomerJanuary: json['highestSaleToCustomerJanuary'],
+      highestSaleToCustomerFebruary: json['highestSaleToCustomerFebruary'],
+      highestSaleToCustomerMarch: json['highestSaleToCustomerMarch'],
+      highestSaleToCustomerApril: json['highestSaleToCustomerApril'],
+      highestSaleToCustomerMay: json['highestSaleToCustomerMay'],
+      highestSaleToCustomerJune: json['highestSaleToCustomerJune'],
+      highestSaleToCustomerJuly: json['highestSaleToCustomerJuly'],
+      highestSaleToCustomerAugust: json['highestSaleToCustomerAugust'],
+      highestSaleToCustomerSeptember: json['highestSaleToCustomerSeptember'],
+      highestSaleToCustomerOctober: json['highestSaleToCustomerOctober'],
+      highestSaleToCustomerNovember: json['highestSaleToCustomerNovember'],
+      highestSaleToCustomerDecember: json['highestSaleToCustomerDecember'],
+    );
   }
 }
--- aarthik_setu/lib/pages/desktop/forms/business_loans/components/enter_gst_credentials.dart
+++ aarthik_setu/lib/pages/desktop/forms/business_loans/components/enter_gst_credentials.dart
@@ -1,10 +1,29 @@
+import 'package:aarthik_setu/global_components/back_button.dart';
+import 'package:aarthik_setu/global_components/date_picker.dart';
+import 'package:aarthik_setu/global_components/procees_button.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hexcolor/hexcolor.dart';
 import '../../../../../constants/colors.dart';
 import '../../../../../global_components/labelled_text_field.dart';
 
+class BusinessInputUnit {
+  final TextEditingController gstNumberController;
+  final TextEditingController gstUsernameController;
+  final TextEditingController cityController;
+  final TextEditingController totalPurchaseController;
+  final TextEditingController totalSalesController;
+  DateTime? updateOn;
 
+  BusinessInputUnit({
+    required this.gstNumberController,
+    required this.gstUsernameController,
+    required this.cityController,
+    required this.totalPurchaseController,
+    required this.totalSalesController,
+    this.updateOn,
+  });
+}
 
 class EnterGSTCredentials extends StatefulWidget {
   const EnterGSTCredentials({super.key});
@@ -14,120 +33,166 @@ class EnterGSTCredentials extends StatefulWidget {
 }
 
 class _EnterGSTCredentialsState extends State<EnterGSTCredentials> {
-  
-  List<String> businesses = [];
-  
+  List<BusinessInputUnit> businesses = [];
+  int? currentBusinessIndex;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // const SizedBox(height: 20),
-        // const Text(
-        //   "Upload Bank Statement (PDF)",
-        //   style: TextStyle(fontSize: 24),
-        // ),
         const SizedBox(height: 20),
-        Row(
-          children: [
-            for (var i in businesses)
-              Container(
-                margin: const EdgeInsets.only(right: 20),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Text(
-                  i,
-                  style: const TextStyle(fontSize: 18),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+                for (int i = 0; i < businesses.length; i++) ...[
+                  SizedBox(
+                    width: 250,
+                    height: 100,
+                    child: FilledButton.tonal(
+                      onPressed: () {
+                        setState(() {
+                          currentBusinessIndex = i;
+                        });
+                      },
+                      style: ButtonStyle(
+                        padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
+                        shape: WidgetStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                        backgroundColor: WidgetStateProperty.all(AppColors.primaryColorOne.withOpacity(0.6)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(Icons.business, color: Colors.black, size: 30),
+                          const SizedBox(width: 10),
+                          Text("Business ${i + 1}", style: GoogleFonts.poppins(fontSize: 22)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                ],
+              SizedBox(
+                width: 250,
+                height: 100,
+                child: FilledButton.tonal(
+                  onPressed: () {
+                    setState(() {
+                      businesses.add(BusinessInputUnit(
+                        gstNumberController: TextEditingController(),
+                        gstUsernameController: TextEditingController(),
+                        cityController: TextEditingController(),
+                        totalPurchaseController: TextEditingController(),
+                        totalSalesController: TextEditingController(),
+                      ));
+                    });
+                  },
+                  style: ButtonStyle(
+                    padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
+                    shape: WidgetStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                    backgroundColor: WidgetStateProperty.all(AppColors.primaryColorOne.withOpacity(0.6)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.add, color: Colors.black, size: 30),
+                      const SizedBox(width: 10),
+                      Text("Add Business", style: GoogleFonts.poppins(fontSize: 22)),
+                    ],
+                  ),
                 ),
               ),
-            SizedBox(
-              width: 250,
-              height: 100,
-              child: FilledButton.tonal(
-                onPressed: () {
-                  setState(() {
-                    //  _formOpened = true;
-                  });
-                },
-                style: ButtonStyle(
-                  padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
-                  shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
-                  backgroundColor:
-                  WidgetStateProperty.all(AppColors.primaryColorOne.withOpacity(0.6)),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(Icons.add, color: Colors.black, size: 30),
-                    const SizedBox(width: 10),
-                    Text("Add Business", style: GoogleFonts.poppins(fontSize: 22)),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
         const SizedBox(height: 20),
         const Divider(color: Colors.grey, thickness: 0.5),
         const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            LabelledTextField(
-              label: "GST Number*",
-              hintText: "Enter GST Number",
-              controller: TextEditingController(),
-            ),
-            LabelledTextField(
-              label: "GST Username*",
-              hintText: "Enter GST Username",
-              controller: TextEditingController(),
-            ),
-          ],
-        ),
-        const SizedBox(height: 40),
-        Divider(color: Colors.grey, thickness: 0.5),
+        if (currentBusinessIndex != null)
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LabelledTextField(
+                    label: "GST Number*",
+                    hintText: "Enter GST Number",
+                    controller: businesses[currentBusinessIndex!].gstNumberController,
+                  ),
+                  LabelledTextField(
+                    label: "GST Username*",
+                    hintText: "Enter GST Username",
+                    controller: businesses[currentBusinessIndex!].gstUsernameController,
+                  ),
+                  LabelledTextField(
+                    label: "City*",
+                    hintText: "Enter City",
+                    controller: businesses[currentBusinessIndex!].cityController,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LabelledTextField(
+                    label: 'Total Purchase*',
+                    hintText: "Enter Total Purchase",
+                    controller: businesses[currentBusinessIndex!].totalPurchaseController,
+                  ),
+                  LabelledTextField(
+                    label: 'Total Sales*',
+                    hintText: "Enter Total Sales",
+                    controller: businesses[currentBusinessIndex!].totalSalesController,
+                  ),
+                  DatePickerButton(
+                    current: businesses[currentBusinessIndex!].updateOn,
+                    label: 'Update On*',
+                    onDateSelected: (date) {
+                      setState(() {
+                        businesses[currentBusinessIndex!].updateOn = date;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            businesses.removeAt(currentBusinessIndex!);
+                            currentBusinessIndex = null;
+                          });
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(AppColors.primaryColorOne.withOpacity(0.35)),
+                          shape:
+                              WidgetStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                        ),
+                        icon: const Icon(Icons.delete, color: Colors.red, size: 30)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        if (currentBusinessIndex == null)
+          Center(child: Text("Select a business to edit", style: GoogleFonts.poppins(fontSize: 22))),
+        const SizedBox(height: 20),
+        const Divider(color: Colors.grey, thickness: 0.5),
         const SizedBox(height: 40),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: FilledButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.white),
-                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    side: BorderSide(color: HexColor("#568737")),
-                  )),
-                ),
-                child: Text(
-                  "Back",
-                  style: TextStyle(fontSize: 20, color: HexColor("#568737")),
-                ),
-              ),
-            ),
+            BackButtonCustom(onPressed: () => context.pop()),
             const SizedBox(width: 40),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: FilledButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(HexColor("#568737")),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "Proceed",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
+            ProceedButtonCustom(onPressed: () {}),
           ],
         )
       ],
