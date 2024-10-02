c4884ac5fb46d1710a52daa2b498da463de1044b
nailsonseat <aadarshv3@gmail.com>
2024-10-02 13:10:30 +0530

    Use development environment

--- aarthik_setu/lib/constants/app_constants.dart
+++ aarthik_setu/lib/constants/app_constants.dart
@@ -2,7 +2,8 @@ class AppConstants {
   static const String appName = 'Aarthik Setu';
   static const double desktopScaleWidth = 1980;
   static const double mobileScaleWidth = 450;
-  static const String backendDomain = 'https://aarthik-setu.onrender.com/';
+  // static const String backendDomain = 'https://aarthik-setu.onrender.com/';
+  static const String backendDomain = 'http://localhost:8080/';
 
   static Duration otpTimeOutDuration = const Duration(minutes: 2);
 }
