fb2afc53a38e1439df826fb3f5100ac4727ad427
nailsonseat <aadarshv3@gmail.com>
2024-10-04 17:09:21 +0530

    form filler repository

---
+++ aarthik_setu/lib/repository/form_filler.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:logger/logger.dart';
+import '../constants/app_constants.dart';
+
+class FormFillerRepository {
+  final Dio _client = Dio(
+    BaseOptions(
+      baseUrl: AppConstants.backendDomain,
+    ),
+  );
+
+  final Logger _logger = Logger();
+
+  Future<Map<String, dynamic>> fillForm(PlatformFile audioFile, List<Map<String, dynamic>> formFields) async {
+    try {
+      final formData = FormData.fromMap({
+        'audioFile': MultipartFile.fromBytes(audioFile.bytes!, filename: 'audio.mp3'),
+        'formFields': formFields,
+      });
+
+      final response = await _client.post('/gen_ai/audio-fill', data: formData);
+      return response.data;
+    } catch (e) {
+      _logger.e('Error form filler: $e');
+      return {};
+    }
+  }
+}
