c895698e9c1ae63e2a9527ab486737e7187401a4
nailsonseat <aadarshv3@gmail.com>
2024-10-04 17:10:12 +0530

    business form fields initial commit

---
+++ aarthik_setu/lib/repository/components/business_loan_form_fields.dart
@@ -0,0 +1,19 @@
+
+class FormField{
+  final String name;
+  final String type;
+  final String? format;
+  final List<String>? options;
+
+  FormField({
+    required this.name,
+    required this.type,
+    this.format,
+    this.options,
+  });
+}
+
+class BusinessLoanFormFields{
+
+
+}
