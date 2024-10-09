9240fe1da91ff57fb22e96cc8205e1f6dfbdd4a6
harishsahadev <harishsahadev@gmail.com>
2024-10-10 01:55:15 +0530

    Dashboard route updated to include mobile and desktop dash

--- aarthik_setu/lib/routes/router.dart
+++ aarthik_setu/lib/routes/router.dart
@@ -1,3 +1,4 @@
+import 'package:aarthik_setu/pages/breakpoints/screen_builder.dart';
 import 'package:aarthik_setu/pages/breakpoints/sign_in.dart';
 import 'package:aarthik_setu/pages/desktop/forms/business_loans/gst_form.dart';
 import 'package:aarthik_setu/pages/desktop/forms/template/loan_form_journey_template.dart';
@@ -19,8 +20,10 @@ import '../pages/desktop/forms/personal_loans/contact_details.dart';
 import '../pages/desktop/forms/personal_loans/employment_details.dart';
 import '../pages/desktop/forms/personal_loans/review_form.dart';
 import '../pages/desktop/loading.dart';
+import '../pages/mobile/home/dashboard.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/dashboard',
   routes: [
     GoRoute(path: '/', pageBuilder: (context, state) => const MaterialPage(child: LoadingPage())),
     GoRoute(
@@ -29,7 +32,7 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: '/dashboard',
-      pageBuilder: (context, state) => const MaterialPage(child: DashboardDesktop()),
+      pageBuilder: (context, state) => const MaterialPage(child: ScreenBuilder(mobile: DashboardMobile(), desktop: DashboardDesktop())),
     ),
     GoRoute(
       path: '/personal-loan-journey/:loanType',
